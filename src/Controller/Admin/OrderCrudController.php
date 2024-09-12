816d4844da56b087dbd950f953ec4fbeb411d8d9
Rudolph79 <rudolph.ebang@gmail.com>
2024-09-12 10:33:34 +0200

    Change Label for admin command

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -18,10 +20,20 @@ class OrderCrudController extends AbstractCrudController
         return Order::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Commande')
+            ->setEntityLabelInPlural('Commandes');
+    }
+
     public function configureActions(Actions $actions): Actions
     {
         return $actions
-                    ->add('index', 'detail');
+            ->add('index', 'detail');
+        // ->remove(Crud::PAGE_INDEX, Action::NEW)
+        // ->remove(Crud::PAGE_INDEX, Action::EDIT)
+        // ->remove(Crud::PAGE_INDEX, Action::DELETE);
     }
 
     public function configureFields(string $pageName): iterable
