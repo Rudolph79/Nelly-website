6102a66bce92b6ab52d41d0c496cd8c46cc59571
Rudolph79 <rudolph.ebang@gmail.com>
2024-09-17 10:43:59 +0200

    Redirection to the homepage if you ain't admin user

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+// src/Security/AccessDeniedHandler.php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        // Redirige vers la page d'accueil ou toute autre page
+        return new RedirectResponse($this->router->generate('app_home'));
+    }
+}
