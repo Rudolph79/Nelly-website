138bf857d9c2a2b1a3cb8e29d3c48eb2fd3de866
Rudolph79 <rudolph.ebang@gmail.com>
2024-09-17 10:22:32 +0200

    Orders in detail in Admin section and France flag added

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -6,9 +6,11 @@ use App\Entity\Order;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -29,17 +31,30 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        $show = Action::new('Afficher')->linkToCrudAction('show');
+
         return $actions
-            ->add('index', 'detail');
-        // ->remove(Crud::PAGE_INDEX, Action::NEW)
-        // ->remove(Crud::PAGE_INDEX, Action::EDIT)
-        // ->remove(Crud::PAGE_INDEX, Action::DELETE);
+            // ->add('index', 'detail');
+            ->add(Crud::PAGE_INDEX, $show)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE);
+    }
+
+    public function show(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+
+        return $this->render('admin/order.html.twig', [
+            'order' => $order
+        ]);
     }
 
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
+            // DateField::new('createdAt')->setLabel('Date'),
             // DateTimeField::new('createdAt','Passée le'),
             TextField::new('user.fullname', 'Utilisateur'),
             MoneyField::new('total')->setCurrency('XAF'),
